21fce8bfa4d70721b4d23b44ca4e39eedf795b8c
GusGus999 <guzmanmartinezangelgustavo@gmail.com>
2025-10-12 14:57:55 -0600

    Modificar para poder visualizar los datos en la tabla
    Signed-off-by: GusGus999 <guzmanmartinezangelgustavo@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,12 @@ module com.example.pseudoaleatorios {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // AÑADE ESTA LÍNEA PARA EL MODELO
+    opens com.example.modelo to javafx.base;
 
+    // Y ASEGÚRATE DE TENER ESTA PARA EL CONTROLADOR
     opens com.example.pseudoaleatorios to javafx.fxml;
+
+    // Es posible que necesites exportar el paquete del controlador si no lo tienes
     exports com.example.pseudoaleatorios;
 }
